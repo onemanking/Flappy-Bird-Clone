62f0ee86d29ad6dc0ccb117748ee73a18190eccd
onemanking <onemanking@hotmail.com>
2025-08-22 12:29:00 +0700

    Implement game state handling in GroundParallax for improved background management

--- Assets/Scripts/GroundParallax.cs
+++ Assets/Scripts/GroundParallax.cs
@@ -4,8 +4,49 @@ public class GroundParallax : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer[] m_SpriteRenderers;
 
+    private Vector2[] m_initialPositions;
+
+    private bool isActive;
+    private void Awake()
+    {
+        m_initialPositions = new Vector2[m_SpriteRenderers.Length];
+        for (var i = 0; i < m_SpriteRenderers.Length; i++)
+        {
+            m_initialPositions[i] = m_SpriteRenderers[i].transform.position;
+        }
+
+        EventBus.GameStateChanged += HandleGameStateChanged;
+    }
+
+    private void HandleGameStateChanged(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.Idle:
+                HandleRestart();
+                break;
+            case GameState.Starting:
+                isActive = true;
+                break;
+            case GameState.GameOver:
+                isActive = false;
+                break;
+        }
+    }
+
+    private void HandleRestart()
+    {
+        isActive = false;
+        for (var i = 0; i < m_SpriteRenderers.Length; i++)
+        {
+            m_SpriteRenderers[i].transform.position = m_initialPositions[i];
+        }
+    }
+
     private void Update()
     {
+        if (!isActive) return;
+
         for (var i = 0; i < m_SpriteRenderers.Length; i++)
         {
             var sr = m_SpriteRenderers[i];
@@ -16,5 +57,10 @@ public class GroundParallax : MonoBehaviour
                 sr.transform.position = targetPos;
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        EventBus.GameStateChanged -= HandleGameStateChanged;
     }
 }
